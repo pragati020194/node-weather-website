abf35d851058bb3223799fce985f92ac468aef09
pragati vashishtha <vashishthapragati2@gmail.com>
2021-05-29 13:03:53 +0530

    console updation

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,10 @@ const forecast = (longitude, latitude , callback) => {
         } else if (body.error){
             callback('Unable to find location. Try another search', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. There is a ' + body.current.precip + '% chance of rain.')
+            console.log(body.daily.data[0])
+            callback(undefined, body.current.weather_descriptions[0] + 
+            '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike 
+            + ' degrees out. There is a ' + body.current.precip + '% chance of rain.')
         }
     })
 }
